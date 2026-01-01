bb1f4df8cbe04cf6d74ba24fa90e965c636fc3cc
Harish Senthilkumar <harishsenthilkmr@gmail.com>
2025-12-31 18:50:08 -0800

    brought in all necesssary libaries for my weather app and making the gui

--- WeatherApp.py
+++ WeatherApp.py
@@ -1,4 +1,17 @@
-print("Hello")
-print("This update is a testing in process")
-p_x=3
-print("exit code    ")
+import sys 
+import requests
+from PyQt5.QtWidgets import QApplication, QWidget, QVBoxLayout, QLabel, QLineEdit, QPushButton, QMessageBox
+from PyQt5.QtCore import Qt
+
+class WeatherApp(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.city_label = QLabel("Enter the City Name:", self)
+        self.city_input = QLineEdit(self)
+        self.get_weather_button = QPushButton("Get Weather", self)
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    weather_app = WeatherApp()
+    weather_app.show()
+    sys.exit(app.exec_())
